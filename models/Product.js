839a97f39679cce3a9a9957d9a376b653845591c
Rebecca Overton <rebeccaoverton@Rebeccas-MacBook-Air.local>
2022-11-03 22:01:22 -0700

    product fields outlined

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,11 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id:{},
+    product_name: {},
+    price: {},
+    stock: {},
+    category_id: {},
   },
   {
     sequelize,
