8c30667dd966fd599217914f6b355fcd561049a1
Rebecca Overton <rebeccaoverton@Rebeccas-MacBook-Air.local>
2022-11-04 00:03:59 -0700

    product get by ID route fixed

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -26,7 +26,7 @@ router.get('/:id', async (req, res) => {
     //search for data by primary key
     const productData = await Product.findByPk(req.params.id,{
       //includes associated tags, join through product tag
-      include: [{model: Tag, through: ProductTag, as: 'tag_product'}]
+      include: [{model: Tag}]
     });
     if(!productData){
       res.status(404).json({message:"Can't retrieve product: id not found."});
