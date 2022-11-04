2886a581bb1ac3018a06d9a21841af07a4f4cc04
Rebecca Overton <rebeccaoverton@Rebeccas-MacBook-Air.local>
2022-11-03 19:17:19 -0700

    sequelize connection added to server

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const routes = require('./routes');
+
 // import sequelize connection
+const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -8,9 +10,12 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
+//turn on routes
 app.use(routes);
 
+
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, ()=>console.log(`App listening on port ${PORT}!`));
 });
