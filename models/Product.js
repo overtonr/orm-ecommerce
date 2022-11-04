2c745cb804bc85279ad9c2c5e0c476bcd2528b17
Rebecca Overton <rebeccaoverton@Rebeccas-MacBook-Air.local>
2022-11-03 22:06:08 -0700

    product model added with comments

--- models/Product.js
+++ models/Product.js
@@ -9,11 +9,32 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    id:{},
-    product_name: {},
-    price: {},
-    stock: {},
-    category_id: {},
+    id:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      isDecimal: true,
+  
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      //default val 10
+      isNumeric: true,
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      //references category models id
+    },
   },
   {
     sequelize,
